867dd38e61010816f099235025e73e934101dec2
jetong <jose.e.tong@gmail.com>
2018-11-28 20:11:38 -0800

    first commit. connected node backend to react frontend

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors'); // addition we make
+var getJSON = require('get-json');
+
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+require('dotenv').config();
+
+// Use CORS
+app.use(cors());
+
+app.use('/calling', (req, res) => {
+	let name = req.query.name.toLowerCase();
+	const url = `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${name}?api_key=${process.env.REACT_APP_API_KEY}`;
+	getJSON(url, function(error, data) {
+		console.log(data);
+		res.send(data);
+	});
+});
+
+//app.use('/calling', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+	username: "",
+    id: null
+  };
+
+  handleInput = (event) => {
+	this.setState({ username: event.target.value});
+  }
+
+  getSummonerId = (event) => {
+	event.preventDefault();
+	const url = `http://localhost:4000/calling?name=${this.state.username}`;
+    fetch(url)
+		.then(response => response.json())
+		.then(myJson => this.setState({ id: JSON.stringify(myJson.id) }));
+	};
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Username: {this.state.username}</p>
+          <p>ID: {this.state.id}</p>
+      		<form onSubmit={this.getSummonerId}>
+        	  <label>
+          		Name:
+          		<input type="text" value={this.state.username} placeholder="Enter summoner name" onChange={this.handleInput}/>
+        	  </label>
+			  <br></br>
+			  <br></br>
+        	  <input type="submit" value="Submit" />
+      		</form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
